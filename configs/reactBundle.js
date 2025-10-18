ae42b2b8eed0b64a9788fe8a94e6434ccf4e9895
eserven <emily.e.serven@gmail.com>
2025-10-17 23:19:00 -0400

    update react bundle config

--- configs/reactBundle.js
+++ configs/reactBundle.js
@@ -5,7 +5,16 @@ import tanstackQueryConfig from "./tanstackQuery.js";
 import tanstackRouterConfig from "./tanstackRouter.js";
 
 export default defineConfig([
-  ...reactConfig,
-  ...tanstackQueryConfig,
-  ...tanstackRouterConfig,
+  {
+    files: ["packages/client/src/**/*.{js,jsx,ts,tsx}"],
+    extends: [...tanstackQueryConfig, ...tanstackRouterConfig],
+  },
+  {
+    files: ["packages/client/src/**/*.{jsx,tsx}"],
+    extends: [...reactConfig],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
 ]);
