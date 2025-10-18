cc0e3248b3ab4ad37fabd2df192a31b57e47a035
eserven <emily.e.serven@gmail.com>
2025-10-17 22:16:40 -0400

    Using the individual configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import main from "./index.js";
-import {defineConfig} from "eslint/config";
+import { defineConfig } from "eslint/config";
+
+import importConfig from "./configs/import.js";
+import stylisticConfig from "./configs/stylistic.js";
+
 export default defineConfig([
-    {
-        files: ["**/*.js"],
-        extends: [main],
-    },
-])
+  ...stylisticConfig,
+  ...importConfig,
+]);
