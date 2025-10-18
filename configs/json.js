81c8729adae00652c613f479a890f6c0a70a3f11
eserven <emily.e.serven@gmail.com>
2025-10-18 10:43:11 -0400

    fixed different import error

--- configs/json.js
+++ configs/json.js
@@ -1,7 +1,11 @@
+import stylistic from "@stylistic/eslint-plugin";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
+    plugins: {
+      "@stylistic": stylistic,
+    },
     rules: {
       "@stylistic/quote-props": 0,
       "@stylistic/semi": 0,
