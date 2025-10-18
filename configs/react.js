3fa7589fa08472a811c933718892969fc10809c5
eserven <emily.e.serven@gmail.com>
2025-10-17 22:29:54 -0400

    added react config

---
+++ configs/react.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "eslint/config";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+
+export default defineConfig([
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat["jsx-runtime"],
+  reactHooks.configs["recommended-latest"],
+  reactRefresh.configs.vite,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    plugins: {
+      react: reactPlugin,
+    },
+  },
+  {
+    rules: {
+      "react/jsx-first-prop-new-line": ["warn", "multiline"],
+    },
+  },
+]);
